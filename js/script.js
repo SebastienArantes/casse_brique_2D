6254a7cd29a467c1d120ace3da110aa68778a24c
Sébastien Arantes <sebastien.arantes@oclock.school>
2025-06-27 14:53:39 +0200

    gestion du rebond de la balle + changement de couleur au rbond mais ça c'est juste pour le fun

--- js/script.js
+++ js/script.js
@@ -4,11 +4,27 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
+let colors = ["#00e1ff", "#6003f5", "#ff0202", "#eeff00", "#000", "#1cf84c"];
+let currentColor = colors[0];
+let lastColor = currentColor;
+
+function getRandomColor() {
+    let newColor;
+    for (let color of colors) {
+        if (color !== currentColor && color !== lastColor) {
+            newColor = color;
+            break;
+        }
+    }
+    lastColor = currentColor;
+    return newColor;
+}
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = currentColor;
     ctx.fill();
     ctx.closePath();
 }
@@ -16,6 +32,17 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    //gestion du rebond
+    // si la balle touche le bord supérieur ou inférieur
+    if (y + dy > canvas.height - ballRadius|| y + dy < ballRadius) {
+        dy = -dy;
+        currentColor = getRandomColor();
+    }
+    // si la balle touche le bord gauche ou droit
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+        currentColor = getRandomColor();
+    }
     x += dx;
     y += dy;
 }
